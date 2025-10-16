1696849b62ec83fae37378f10e4bbb2e8df948f4
unknown <Sophie>
2025-10-16 08:59:18 -0400

    hola

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -2,32 +2,51 @@
 ##### Template for Chapter 5.14, Exercises 1 - 4 ######
 
 
-print("********** Ch 5 Exercise 1 **********")
+# print("********** Ch 5 Exercise 1 **********")
 
-def time_since_epoch():
-    import time
-    T=  time.time()
-    print(T)
-    minutes =int (T // 60)
-    print (minutes)
-time_since_epoch()
+# def time_since_epoch():
+#     import time
+#     T=  time.time()
+#     print(T)
+#     minutes =int (T // 60)
+#     print (minutes)
+# time_since_epoch()
     
 
 
 
-print("********** Ch 5 Exercise 2 **********")
-
-# Do your work for Excercise 2 here.
-
-print("Ch 5 Exercise 2: Not implemented") # Delete this line when you write your code!
+#  print("********** Ch 5 Exercise 2 **********")
 
 
 
-print("********** Ch 5 Exercise 3 **********")
 
-# Do your work for Exercise 3 here.
 
-print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
+# print("********** Ch 5 Exercise 3 **********")
+
+def is_triangle(a, b, c):
+    print('is_triangle()', a, b, c)
+    if a>=b + c:
+        print('NO')
+    elif b>= a + c:
+        print('NOOO')
+    elif c>= a + b:
+         print('NONONO')
+    else:
+        print("yes!")
+    
+
+a = float(input('How long is side a?'))
+print('a is', a, type(a)) 
+
+b = float(input('How long is side b?'))
+print('b is', b, type(b))
+
+c = float(input('How long is side c?'))
+print('c is', c, type(c))
+
+is_triangle(a ,b ,c )
+
+# print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
 
 
 
@@ -35,4 +54,4 @@ print("********** Ch 5 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
 
-print("Ch 5 Exercise 4: Not implemented") # Delete this line when you write your code!
+# print("Ch 5 Exercise 4: Not implemented") # Delete this line when you write your code!
--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -2,11 +2,21 @@
 ##### Template for Homework 1, Exercises 1-3 ######
 
 
-print("********** Homework 1 Exercise 1 **********")
+
+def tic_tac_toe():
+    v_row =(' '*2 + '|')*2+ ' '*2
+    h_row = '-'*8
+    print(v_row)
+    print(h_row)
+    print(v_row)
+    print(h_row)
+    print(v_row)
+
+tic_tac_toe()
 
 # Do your work for Exercise 1 here
 
-print("Homework 1 Exercise 1: Not implemented") # Delete this line when you write your code!
+# print("Homework 1 Exercise 1: Not implemented") # Delete this line when you write your code!
 
 
 
@@ -14,7 +24,7 @@ print("********** Homework 1 Exercise 2 **********")
 
 # Do your work for Excercise 2 here
 
-print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
+# print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
 
 
 
@@ -22,4 +32,4 @@ print("********** Homework 1 Exercise 3 **********")
 
 # Do your work for Excercise 3 here
 
-print("Homework 1 Exercise 3: Not implemented") # Delete this line when you write your code!
+# print("Homework 1 Exercise 3: Not implemented") # Delete this line when you write your code!
