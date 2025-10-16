b154fd1d3e9c18f547c76a858a6f46d88cab7fcb
SophieCarter <Sophie>
2025-10-16 09:59:46 -0400

    week 9 work

--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -3,16 +3,8 @@
 
 
 
-def tic_tac_toe():
-    v_row =(' '*2 + '|')*2+ ' '*2
-    h_row = '-'*8
-    print(v_row)
-    print(h_row)
-    print(v_row)
-    print(h_row)
-    print(v_row)
+#
 
-tic_tac_toe()
 
 # Do your work for Exercise 1 here
 
@@ -20,16 +12,22 @@ tic_tac_toe()
 
 
 
-print("********** Homework 1 Exercise 2 **********")
 
-# Do your work for Excercise 2 here
+def your_name():
 
-# print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
-
+    first=input('Enter your first name')
+    second=input('Enter your last name')
+    third=input('What month were you born?')
+    fourth=input('What day?')
+    fifth=input('What year?')
+    print(first, second, 'was born', third, fourth, fifth)
+# your_name()
 
 
 print("********** Homework 1 Exercise 3 **********")
 
 # Do your work for Excercise 3 here
 
-# print("Homework 1 Exercise 3: Not implemented") # Delete this line when you write your code!
+def_rules():
+
+
---
+++ chapters/05_2_homework/rsp.py
@@ -0,0 +1,2 @@
+# def_rock_paper_scissors
+# if (player1 == 'rock' and player2 == 'scissors'):
