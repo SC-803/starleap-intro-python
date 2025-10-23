ef2acf62d14400944527f22641bfc64e1715270f
SophieCarter <Sophie>
2025-10-23 09:56:27 -0400

    week 10 work

--- chapters/05_2_homework/rsp.py
+++ chapters/05_2_homework/rsp.py
@@ -1,2 +1,31 @@
-# def_rock_paper_scissors
-# if (player1 == 'rock' and player2 == 'scissors'):
+import pwinput
+
+
+
+
+def rock_paper_scissors_game(Player1, Player2):
+    
+    if (Player1== 'rock' and Player2== 'scissors'):
+        print('Player1 wins!')
+    elif (Player1== 'paper' and Player2== 'rock'):
+        print('Player1 wins!')
+    elif (Player1== 'scissors'and Player2== 'paper'):
+        print('Player1 wins!')
+
+    elif (Player1== Player2):
+        print('DRAW!')
+
+    else:
+        print('Player2 wins!')
+
+# def validate(val):
+
+def main():
+    Player1=input('Player1 enter ')
+    # validate(Player1)                               
+    Player2=input('Player2 enter ')
+    
+    
+
+    rock_paper_scissors_game(Player1, Player2)
+main()
