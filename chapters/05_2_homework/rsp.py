c43f4e51536dbce59bde52bb9da93ded2c6f3b78
SophieCarter <Sophie>
2025-11-12 20:30:21 -0500

    new work nov 12

--- chapters/05_2_homework/rsp.py
+++ chapters/05_2_homework/rsp.py
@@ -1,31 +1,35 @@
-import pwinput
-
-
-
+import getpass
+import time
 
 def rock_paper_scissors_game(Player1, Player2):
-    
-    if (Player1== 'rock' and Player2== 'scissors'):
+    if Player1 == 'rock' and Player2 == 'scissors':
         print('Player1 wins!')
-    elif (Player1== 'paper' and Player2== 'rock'):
+    elif Player1 == 'paper' and Player2 == 'rock':
         print('Player1 wins!')
-    elif (Player1== 'scissors'and Player2== 'paper'):
+    elif Player1 == 'scissors' and Player2 == 'paper':
         print('Player1 wins!')
-
-    elif (Player1== Player2):
+    elif Player1 == Player2:
         print('DRAW!')
-
     else:
         print('Player2 wins!')
 
-# def validate(val):
-
 def main():
-    Player1=input('Player1 enter ')
-    # validate(Player1)                               
-    Player2=input('Player2 enter ')
-    
-    
+    print("\nWelcome to Rock, Paper, Scissors!")
+    print("Type 'rock', 'paper', or 'scissors'. Let's begin!\n")
+    time.sleep(1)
 
-    rock_paper_scissors_game(Player1, Player2)
+    Player1 = getpass.getpass('Player1, enter your choice (hidden): ')
+    Player2 = getpass.getpass('Player2, enter your choice (hidden): ')
+
+    print("\nRock...")
+    time.sleep(0.6)
+    print("Paper...")
+    time.sleep(0.6)
+    print("Scissors...")
+    time.sleep(0.6)
+    print("SHOOT!\n")
+    time.sleep(0.4)
+
+    rock_paper_scissors_game(Player1.lower(), Player2.lower())
+
 main()
